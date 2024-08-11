b99281e5d69210676e85eee46510224121e7ab4d
lsconnect <c.s.leandro@live.com>
2024-08-11 01:31:14 -0300

    teste pwa

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 import { App } from "./scripts/App.js";
 
 App.init();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado com sucesso:", registration);
+      })
+      .catch((error) => {
+        console.log("Falha ao registrar o Service Worker:", error);
+      });
+  });
+}
